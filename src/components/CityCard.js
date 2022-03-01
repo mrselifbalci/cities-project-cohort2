6b7e8b39aaba745200a98ee7b12a96667fea91c1
Elif Balci <mrselifbalci@gmail.com>
2022-03-01 19:44:11 +0000

    first commit

---
+++ src/components/CityCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function CityCard({city}) {
+  return (
+    <div className='city-card'> 
+       <img src={city.imageUrl} className='city-image' alt="city"/>
+       <h3>{city.cityName.toUpperCase()}</h3>
+       <p>{city.description}</p>
+       <a href={`/citydetails/${city.cityName}`}><button>See Details</button></a>
+    </div>
+  )
+}
